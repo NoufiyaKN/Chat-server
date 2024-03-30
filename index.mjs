bfc3db298125a983ad08a1a1818b111e296e567d
unknown <noufiyashefi@gmail.com>
2024-03-30 17:12:09 +0530

    Chat server first commit

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port : 8080 })
+server.on('connection',socket => 
+    socket.on('message',data =>
+        server.clients.forEach(client => 
+            client.send(data))
+    )
+)
